d8d1d1e1f056170cf7cff227a2acef19c4677bdf
Minh Sáng <sangthptahh@gmail.com>
2026-02-04 08:14:49 +0700

    ...

--- RecruitmentCVScreening.WinForms/Program.cs
+++ RecruitmentCVScreening.WinForms/Program.cs
@@ -14,6 +14,6 @@ public static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        System.Windows.Forms.Application.Run(new MainForm());
+        System.Windows.Forms.Application.Run(new UploadCVForm());
     }
 }
