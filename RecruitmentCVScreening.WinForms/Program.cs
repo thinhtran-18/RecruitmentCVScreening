0596eed3ff098d9c91c4df486d256fa6861a3daa
sikk0523 <sick2hai@gmail.com>
2026-03-25 21:37:56 +0700

    đã code phần application và chỉnh 1 số database

--- RecruitmentCVScreening.WinForms/Program.cs
+++ RecruitmentCVScreening.WinForms/Program.cs
@@ -15,7 +15,9 @@ public static class Program
       // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        System.Windows.Forms.Application.Run(new UploadCVForm());
+        //System.Windows.Forms.Application.Run(new UploadCVForm());
+        System.Windows.Forms.Application.Run(new ApplicationsForm());
+
     }
 }
 
